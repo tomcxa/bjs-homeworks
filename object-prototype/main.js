863de42535317a9feacb128eb9a8f59b4acb5e42
Anton Golikov <tomcxa@yandex.ru>
2019-11-14 14:36:12 +0300

    first try

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,14 +1,5 @@
-function initCheckBirthday() {
-    const birthday = document.getElementById('birthday').value;
+'use strict';
 
-    const result = checkBirthday(birthday) ? "Да" : "Нет";
-
-    document.getElementById('disclaimer').innerHTML = result;   
-}
-
-function checkBirthday(birthday) {
-    // код для задачи №1 писать здесь
-}
 
 function initPrintAnimalSound() {
     const animal = {
@@ -17,11 +8,16 @@ function initPrintAnimalSound() {
 
     const result = getAnimalSound(animal);
 
-    document.getElementById('sound').innerHTML = result;   
+    document.getElementById('sound').innerHTML = result;
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №2 писать здесь
+    // код для задачи №1 писать здесь
+    if (!animal) {
+        return null;
+    }
+    const sound = animal.sound;
+    return sound;
 }
 
 function initCalculateStatement() {
@@ -35,5 +31,35 @@ function initCalculateStatement() {
 }
 
 function getAverageMark(marks) {
+    // код для задачи №2 писать здесь
+    //невыразительный Я
+    // let sum = 0;
+    // for (let mark of marks) {
+    //    let num = Number(mark);
+    //    sum += num;
+    // }
+    // let roundedAverage = Math.round(sum / marks.length);
+    //выразительный js
+    const roundedAverage = Math.round(marks.reduce((sum, mark) => {
+        return sum + Number(mark)
+    }, 0) / marks.length);
+    return roundedAverage;
+}
+
+function initCheckBirthday() {
+    const birthday = document.getElementById('birthday').value;
+
+    const result = checkBirthday(birthday) ? "Да" : "Нет";
+
+    document.getElementById('disclaimer').innerHTML = result;
+}
+
+function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
+    const now = new Date();
+    const dateOfBirthday = new Date(birthday);
+    const averageMsInYear = 31556952000;
+    const age = (now - dateOfBirthday) / averageMsInYear;
+    console.log(age);
+    return (age > 18);
 }
